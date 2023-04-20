0687904afac6c51cb6a9e23f88e8966333104c4c
ShylaOommen <124379324+ShylaOommen@users.noreply.github.com>
2023-04-20 15:35:09 +0000

    updated results, linter error fixing

--- results.php
+++ results.php
@@ -4,38 +4,44 @@
 
   $userAge = $_POST['user-age'];
 
+// response for when user does not enter an age
+
+if (empty($userAge)) {
+      echo "Please enter your age. Thank you for using this tool!";
+}
+
 // response for when user age is less than or equal to 0 
 
-if ($userAge <= 0) {
-      echo "Please enter a valid age.";
+elseif ($userAge <= 0) {
+      echo "Please enter a valid age. Thank you for using this tool!";
 } 
 
 // response for when user age is 17 or greater
 
-else if ($userAge >= 17) {
-      echo "You can see an R rated movie alone.";
+elseif ($userAge >= 116) {
+      echo "You must be alive to watch a movie. Thank you for using this tool!";
+}
+
+// response for when user age is 17 or greater
+
+elseif ($userAge >= 17) {
+      echo "You can see an R rated movie alone. Thank you for using this tool!";
 }
 
 // response for when user age is 13 or greater (but less than 17)
 
-else if ($userAge >= 13) {
-      echo "You can see a PG-13 movie alone.";
+elseif ($userAge >= 13) {
+      echo "You can see a PG-13 movie alone. Thank you for using this tool!";
 }
 
 // response for when user age is 5 or greater (but less than 13)
 
-else if ($userAge >= 5) {
-      echo "You can see a G and PG rated movie alone.";
+elseif ($userAge >= 5) {
+      echo "You can see a G and PG rated movie alone. Thank you for using this tool!";
 }
 
 // response for when user age is less than 5 (greater than 0)
 
-else if ($userAge < 5) {
-      echo "You cannot see any movies alone.";
-}
-
-// response for when user does not enter an age
-
-else if ($userAge = "") {
-      echo "Please enter your age.";
+elseif ($userAge < 5) {
+      echo "You cannot see any movies alone. Thank you for using this tool!";
 }
